6d2ef9f597df364ae424f5b7f0cb321799f4cd02
gmunish007 <gmunish@rocketmail.com>
2025-08-15 17:20:27 +0530

    string reverse by removing spaces

---
+++ src/test/java/StringReverseWithNoSpaces.java
@@ -0,0 +1,36 @@
+// String reverse by removing spaces between them
+// 1. remove spaces between them
+// 2. reverse the string
+
+public class StringReverseWithNoSpaces {
+
+    public static String removeSpace(String str) {
+
+        char[] charArray = str.toCharArray();
+
+        String empStr = "";
+
+        for (int i = 0; i < charArray.length; i++) {
+            if (charArray[i] != ' ') {
+                empStr = empStr + charArray[i];
+            }
+        }
+        return empStr;
+    }
+
+    public static void main(String[] args) {
+
+        String s = "hello workd";
+        String str = removeSpace(s);
+        System.out.println("String after no spaces:" +str);
+
+        String revStr = " ";
+        char revCh;
+
+        for(int i=0; i<str.length(); i++){
+            revCh = str.charAt(i);
+            revStr = revCh+revStr;
+        }
+        System.out.println("Reversed String:"+revStr);
+    }
+}
