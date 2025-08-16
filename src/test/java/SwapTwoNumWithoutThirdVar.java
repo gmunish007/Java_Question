5cb04c9bc2fbf186d52a4bb86be977f96af64ea5
gmunish007 <gmunish@rocketmail.com>
2025-08-16 19:59:23 +0530

    Swap two numbers without using third variable

---
+++ src/test/java/SwapTwoNumWithoutThirdVar.java
@@ -0,0 +1,13 @@
+public class SwapTwoNumWithoutThirdVar {
+
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 7;
+        num2 = num1 + num2; //num2=12
+        num1 = num2 - num1; //12-5=7
+        num2 = num2 - num1; //12-7=5
+
+        System.out.println("Value of Number 1:" + num1);
+        System.out.println("Value of Number 2:" + num2);
+    }
+}
