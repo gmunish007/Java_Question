f2df53b483a73f596be00413e8c5f17f68e7cd1c
gmunish007 <gmunish@rocketmail.com>
2025-08-18 23:41:34 +0530

    selenium pagescroll

---
+++ src/test/java/pageScroll.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class pageScroll {
+
+    public static void main(String[] args){
+        WebDriver driver = new ChromeDriver();
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("window.scrollTo(0,500)"); // scroll to 500 pixels
+        js.executeScript("window.scrollBy(0, 500)"); // scroll up by 500 pixels
+    }
+}
