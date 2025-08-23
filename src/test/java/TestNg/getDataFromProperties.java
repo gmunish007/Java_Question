4d8a55bb90dccbde32a2a1e30cf4bfb3e655c62a
gmunish007 <gmunish@rocketmail.com>
2025-08-23 22:43:57 +0530

    data properties file

---
+++ src/test/java/TestNg/getDataFromProperties.java
@@ -0,0 +1,24 @@
+package TestNg;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class getDataFromProperties {
+
+    @Test
+    public void getData() throws IOException {
+        Properties ps = new Properties();
+        FileInputStream fs = new FileInputStream("D:\\Testing\\Projects\\InterviewQuestions\\src\\test\\java\\TestData.properties");
+        ps.load(fs);
+
+        WebDriver driver = new ChromeDriver();
+        driver.get(ps.getProperty("url"));
+        driver.manage().window().maximize();
+    }
+}
